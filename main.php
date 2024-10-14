baecf529ddb388144ae62f1b4e73050fa3391097
naylor-debian <95220392+naylor-debian@users.noreply.github.com>
2024-10-14 07:54:11 -0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" src="styles.css">
+    <title>Sakaki Web</title>
+</head>
+<body>
+    <h1>Sakaki Services Web </h1>
+    <p></p>
+</body>
+</html>
